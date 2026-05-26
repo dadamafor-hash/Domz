45d1944d7392b7ecd8220c6c4cb7ca2e1a93ccdd
dadamafor-hash <dadamafor@gmail.com>
2026-05-26 15:42:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth - 0.5) * 30;
+  const y = (e.clientY / window.innerHeight - 0.5) * 30;
+
+  document.querySelector(".background").style.transform =
+    `translate(${x}px, ${y}px)`;
+});
